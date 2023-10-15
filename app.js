008e70b9c1b9a7c7f600048352d92e5c26f205fd
Jacob Casas <skedabble@gmail.com>
2023-10-15 16:24:29 -0700

    update

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
+require('./app_api/models/db');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
+const apiRouter = require('./app_api/routes/index');
 
 const app = express();
 
@@ -15,8 +17,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, 'app_server','views/partials'));
-
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
 app.set('view engine', 'hbs');
 
@@ -26,24 +27,47 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('api', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200/');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Acess-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+
+// Add the following code to fix the error message "Error: Route.get() requires a callback function but got a [object Undefined]":
+
+const routeHandler = (req, res) => {
+    res.send('Hello, world!');
+};
+
+const travelRouteHandler = (req, res) => {
+    res.send('Handling GET requests to the /travel path');
+};
+
+app.get('/', routeHandler);
+app.get('/travel', travelRouteHandler);
+
+app.listen(3000);
 
 module.exports = app;
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const controller = require('../controllers/travel');
 
 /* GET home page. */
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
 router.get('/', controller.travel);
 module.exports = router;
